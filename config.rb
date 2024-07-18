a62103929d5b272326d5c5d3d66bfada4e1cb024
Divyanshu Rathore <divyanshu03@live.com>
2024-07-18 17:03:31 +0530

    Updates config with default values

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 require 'selenium-webdriver'
 
 # Set up paths for Selenium to find ChromeDriver and the Chrome binary
-Selenium::WebDriver::Chrome.path = ENV['CHROME_BINARY_PATH']
-Selenium::WebDriver::Chrome::Service.driver_path = ENV['CHROME_DRIVER_PATH']
+Selenium::WebDriver::Chrome.path = ENV.fetch('CHROME_BINARY_PATH', '/usr/bin/chromium')
+Selenium::WebDriver::Chrome::Service.driver_path = ENV.fetch('CHROME_DRIVER_PATH', '/usr/bin/chromedriver')
